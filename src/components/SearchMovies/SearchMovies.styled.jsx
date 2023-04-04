32535b18291b0d84b3b84fb160b34669f09af94a
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-04 21:45:04 +0200

    Create SearchMovies.styled.jsx

---
+++ src/components/SearchMovies/SearchMovies.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Form = styled.form``;
+
+export const Input = styled.input`
+  width: 300px;
+  height: 25px;
+
+  &:hover {
+    border-color: blue;
+  }
+`;
+export const Button = styled.button`
+  height: 25px;
+`;
