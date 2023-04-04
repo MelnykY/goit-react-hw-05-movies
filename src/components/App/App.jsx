213d630196342f4dfe552cf3140daf517dc7e9fa
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-04 21:44:10 +0200

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import Layout from '../Layout/Layout';
+
+const Home = lazy(() => import('../../pages/Home'));
+const Movies = lazy(() => import('../../pages/Movies'));
+const MovieDetails = lazy(() => import('../../pages/MovieDetails'));
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
+const NotFound = lazy(() => import('../NotFound/NotFound'));
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
