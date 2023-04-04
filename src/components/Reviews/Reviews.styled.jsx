24219e2ccf470aac9415249a8a9fec1176472461
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-04 21:44:55 +0200

    Create Reviews.styled.jsx

---
+++ src/components/Reviews/Reviews.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 20px;
+`;
+
+export const Title = styled.h3`
+  text-transform: capitalize;
+`;
+
+export const Text = styled.p``;
