ed8165f0e88f9cb68d3a8d68fc539fee9703079f
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-04 21:44:18 +0200

    Create Cast.jsx

---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieCredits } from '../../API/movieAPI';
+import { Container } from './Cast.styled';
+
+const BASE_URL_IMG = 'https://image.tmdb.org/t/p/w500/';
+
+const Cast = () => {
+  const [state, setState] = useState([]);
+  const [load, setload] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    getMovieCredits(abortController, id)
+      .then(response => {
+        setState(response.data.cast);
+        setload(true);
+      })
+      .catch(error => {
+        return error;
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, [id]);
+
+  return (
+    <Container>
+      {!load ? (
+        <p>loading...</p>
+      ) : (
+        <ul>
+          {state.map(({ profile_path, name, character, id }) => (
+            <li key={id}>
+              <img
+                src={`${BASE_URL_IMG}${profile_path}`}
+                alt={name}
+                style={{ width: 100, height: 150 }}
+              />
+              <p>{name}</p>
+              <p>Character: {character}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Container>
+  );
+};
+
+export default Cast;
