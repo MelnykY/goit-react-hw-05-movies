79c3d22b0d44a3bf8346e4a97be72484e6d0ee91
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-04 21:44:28 +0200

    Create Reviews.jsx

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,46 @@
+import { getMovieReviews } from '../../API/movieAPI';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Container, Title, Text } from './Reviews.styled';
+
+const Reviews = () => {
+  const { id } = useParams();
+  const [state, setSteta] = useState([]);
+  const [load, setload] = useState(false);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    getMovieReviews(abortController, id)
+      .then(response => {
+        setSteta(response.data.results);
+        setload(true);
+      })
+      .catch(error => {
+        return error;
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, [id]);
+
+  return (
+    <div>
+      {!load ? (
+        <p>loading...</p>
+      ) : state.length !== 0 ? (
+        state.map(({ author, content, id }) => (
+          <Container key={id}>
+            <Title>{author}</Title>
+            <Text>{content}</Text>
+          </Container>
+        ))
+      ) : (
+        <p>We don't have any reviews for this movie</p>
+      )}
+    </div>
+  );
+};
+
+export default Reviews;
