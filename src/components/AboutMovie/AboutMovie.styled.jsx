c24d58e6045434e104a3fd7ab0cbf72420bec49f
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-04-04 21:43:55 +0200

    Create AboutMovie.styled.jsx

---
+++ src/components/AboutMovie/AboutMovie.styled.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const MovieDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border-bottom: 1px solid #212121;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  padding: 10px;
+  gap: 10px;
+  border-bottom: 1px solid #212121;
+`;
+
+export const Img = styled.img``;
+
+export const UserScore = styled.p`
+  display: inline-block;
+  margin-right: 5px;
+`;
+
+export const Overview = styled.p``;
+
+export const Title = styled.p`
+  margin-left: 20px;
+`;
